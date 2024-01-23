1e50f97441c8e48ccedf5b8c37ea0c39988e6357
Ivan <zakharovivann@gmail.com>
2024-01-23 12:25:43 +0300

    add chat type filter

--- filter.py
+++ filter.py
@@ -11,3 +11,14 @@ class AdminFilter(BaseFilter):
 
     async def __call__(self, message: Message) -> bool:
         return message.from_user.id == self.admin_list
+
+
+class ChatTypeFilter(BaseFilter):  # [1]
+    def __init__(self, chat_type: Union[str, list]): # [2]
+        self.chat_type = chat_type
+
+    async def __call__(self, message: Message) -> bool:  # [3]
+        if isinstance(self.chat_type, str):
+            return message.chat.type == self.chat_type
+        else:
+            return message.chat.type in self.chat_type
--- router.py
+++ router.py
@@ -3,7 +3,8 @@ import datetime
 
 from aiogram import Router, types
 from aiogram.filters import Command
-from filter import AdminFilter
+from filter import AdminFilter, ChatTypeFilter
+
 
 default_router = Router()
 
@@ -29,12 +30,12 @@ async def get_last_message_time(message: types.Message):
         days, seconds = divmod(difference.total_seconds(), 24 * 3600)
         hours, seconds = divmod(seconds, 3600)
         minutes, seconds = divmod(seconds, 60)
-        await message.reply(f" не писал сообщение {int(days)} дней {int(hours)} часов {int(minutes)} минут {int(seconds)} секунд.")
+        await message.reply(f"@klabukow не писал сообщение в чат {int(days)} дней {int(hours)} часов {int(minutes)} минут {int(seconds)} секунд.")
     else:
         await message.reply("Вы еще не отправляли сообщений.")
 
 
-@default_router.message(AdminFilter(time_user=user))
+@default_router.message(AdminFilter(time_user=user), ChatTypeFilter(chat_type=["group", "supergroup"]))
 async def remember_last_message_time(message: types.Message):
     global last_message
 
