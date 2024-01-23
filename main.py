758622d28bac032bc1cddd6676d2a6bf5e1fa608
Ivan <zakharovivann@gmail.com>
2024-01-23 12:48:52 +0300

    test

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ async def main():
     dp = Dispatcher()
 
     dp.include_router(default_router)
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
--- router.py
+++ router.py
@@ -30,7 +30,7 @@ async def get_last_message_time(message: types.Message):
         days, seconds = divmod(difference.total_seconds(), 24 * 3600)
         hours, seconds = divmod(seconds, 3600)
         minutes, seconds = divmod(seconds, 60)
-        await message.reply(f"@klabukow не писал сообщение в чат {int(days)} дней {int(hours)} часов {int(minutes)} минут {int(seconds)} секунд.")
+        await message.reply(f"@klabukow не писал сообщение в чат {int(days)} дней {int(hours)-3} часов {int(minutes)} минут {int(seconds)} секунд.")
     else:
         await message.reply("Вы еще не отправляли сообщений.")
 
